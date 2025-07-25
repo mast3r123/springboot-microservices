b8ac22641478e5a2572b143995650f47d1923fe9
mast3r123 <abhishek.mast3r@gmail.com>
2025-07-25 16:18:31 -0400

    patient service

---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,21 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patientList = patientRepository.findAll();
+    }
+}
